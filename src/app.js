ef6ab055035508c308c79d3a2ddcf0d4f7900440
Mohamed Nabil <mohd.nabil982@gmail.com>
2021-01-23 16:02:01 +0300

    adding new functions

--- src/app.js
+++ src/app.js
@@ -14,29 +14,38 @@ app.get('/getByName',async (req,res)=>{
     const name=req.query.user ;
     if(!name)
      return res.send({error:"User is not set"}) ;
-     try{
-         const users= await MongoDB.GetUserName(name);
-         res.json(users);
-     }catch(error){
-         res.json({error:error.message});
-     }
+   MongoDB.GetUserName(name).then(results=>{
+    res.json(results) ;
+   }).catch(err=>{
+    res.json(err) ;
+   });
 });
 
 app.get('/getByEmail',(req,res)=>{
     const mail=req.query.mail;
     if(!mail)
-     {
-        return res.json({error:'You must specify an email'}) ;
-     }
-    MongoDB.GetUserByEmail(mail,(err,users)=>{
-        if(err)
-         {
-            res.json(err) ;
-         }else{
-             res.json(users) ;
-         }
+    return res.json({error:'You must specify an email'}) ;
+    MongoDB.GetUserByEmail(mail).then(results=>{
+        res.json(results);
+    }).catch(err=>{
+        res.json(err);
     });
 });
+
+app.get('/getByAge',(req,res)=>{
+    const min =req.query.min ;
+    const max =req.query.max ;
+    if(!min ||!max)
+     return res.json ({error:"Min & Max age must be specified"}) ;
+    MongoDB.GetUserByAge(min,max).then(users=>{
+        res.json(users);
+    }).catch(err=>{
+        res.json(err);
+    })
+});
+
+// MongoDB.UpdateTasks(false);
+// MongoDB.DeleteUsersByAge(0,18);
 app.listen(3000,()=>{
     console.log("Express Is Running");
 })
--- src/utils.js
+++ src/utils.js
@@ -51,103 +51,116 @@ let AddTasks=async (tasks,callback)=>{
   }
 }
 
-let GetUserName=async (name,callback)=>{
-   const MongoClient=new mongodb(connectionURL,{useNewUrlParser:true,useUnifiedTopology:true});
+let GetUserName=async (name)=>{
+   const client=new mongodb(connectionURL,{useNewUrlParser:true,useUnifiedTopology:true});
    try{
-    const conn=await MongoClient.connect();
-    const db=MongoClient.db(databaseName) ;
-    const collection=db.collection('users') ;
-    const documents= await collection.find({}).toArray();
-    const reg=new RegExp(`^${name}`,'i');
-    const users=documents.filter(user=>user.name.search(reg)>=0);
-    if(callback)
-      callback(undefined,users);
-      return users;
+    await client.connect();
+    const regexp=new RegExp(`^${name}.*`,"i");
+    const users=await client.db(databaseName).collection('users').find(
+      {
+        name :regexp
+      }
+    ).toArray();
+    return users;
    }catch(error){
-     if(callback)
-      callback(error,undefined);
-      throw new Error(error);
+    throw new Error (error.message);
    }finally{
-    await MongoClient.close();
+    await client.close();
    }
+   
 };
 
-
-let GetTasks = async (status,callback)=>{
-  const MongoClient=new mongodb(connectionURL,{useNewUrlParser:true,useUnifiedTopology:true});
-  try{
-    const conn= await MongoClient.connect();
-    const db=MongoClient.db(databaseName) ;
-    const collection=db.collection('tasks')  ;
-    const tasks =await collection.find({status}).toArray() ;
-    if(callback)
-      callback(undefined,tasks);
-    return tasks ;
-  }catch(error){
-    if(callback)
-      callback(error,undefined);
-    throw new Error(error);
-  }finally{
-    await MongoClient.close();
-  }
-};
-
-
-let GetUserByAge = async (max=100,min=0,callback)=>{
-  const MongoClient=new mongodb(connectionURL,{useNewUrlParser:true,useUnifiedTopology:true});
-  try{
-    const conn= await MongoClient.connect();
-    const db=MongoClient.db(databaseName) ;
-    const collection=db.collection('users');
-    const allUsers=await collection.find({}).toArray();
-    const users = allUsers.filter(user=>user.age<=max && user.age>=min);
-    if(callback)
-      callback(undefined,users) ;
-    return users ;
-  }catch(error){
-    if(callback)
-      callback(error,undefined);
-    throw new Error(error);
-  }finally{
-    await MongoClient.close();
-  }
-};
-
-const GetUserByEmail =async (email,callback)=>{
+const GetUserByEmail =async (email)=>{
   const client=new mongodb(connectionURL,{useNewUrlParser:true,useUnifiedTopology:true});
   try{
     const conn= await client.connect() ;
-    const allUsers = await client.db(databaseName).collection('users').find({}).toArray();
     const reg=new RegExp(`^${email}`,'i') ;
-    users=allUsers.filter(u=>u.email && u.email.search(reg)>=0);
-    if(callback)
-     {
-       callback(undefined,{count:users.length , users});
-     }
-    else
-     {
-       return users ;
-     }
+    const users = await client.db(databaseName).collection('users').find({
+     email :reg
+    }).toArray();
+    return users;
+  }catch(err){
+    throw new Error (err.message);
+  }finally{
+    await client.close();
+  }
+};
+
+let GetTasks = async (status)=>{
+  const client=new mongodb(connectionURL,{useNewUrlParser:true,useUnifiedTopology:true});
+  try{
+    await client.connect();
+    const tasks=client.db(databaseName).collection('tasks').find(
+      {status}
+    ).toArray();
+      return tasks
   }catch(error){
-    if(callback)
-     {
-       callback({error:error.message}) ;
-     }
-     else
-      {
-        throw new Error({error :error.message}) ;
-      }
+    throw new Error(error.message);
   }finally{
     await client.close();
   }
 };
 
 
+let GetUserByAge = async (min,max)=>{
+  const client=new mongodb(connectionURL,{useNewUrlParser:true,useUnifiedTopology:true});
+  try{
+   const connection = await client.connect();
+   const Max=new Number(max) ; 
+   const Min=new Number(min);
+   const users=await client.db(databaseName).collection('users').find({
+     age:{$lte:Max , $gte: Min}
+   }).toArray();
+    console.log(users)
+    return users ;
+  }catch(error){
+    throw new Error(error.message);
+  }finally{
+    await MongoClient.close();
+  }
+};
+
+
+
+const UpdateTasks = (status)=>{
+  const client=new mongodb(connectionURL,{useNewUrlParser:true , useUnifiedTopology:true}) ;
+  client.connect().then(()=>{
+    return client.db(databaseName).collection('tasks').updateMany(
+      {$and:[{status:false},{name:"Buy Groceries"}]},
+      {$set:{status : true}}
+    )
+  }).then(result=>{
+     console.log(result);
+  }).catch(error=>{
+     console.log(error.message);
+  }).finally(()=>{
+    client.close();
+  });
+};
+
+const DeleteUsersByAge = (min,max)=>{
+  const client=new mongodb(connectionURL,{useNewUrlParser:true,useUnifiedTopology:true}) ;
+  client.connect().then(()=>{
+    return client.db(databaseName).collection('users').deleteMany(
+      {
+        $and : [ {age:{$gte:min}} , {age:{$lte:max}} ]
+      }
+    )
+  }).then(result=>{
+    console.log(result) ;
+  }).catch(error=>{
+    console.log(error);
+  }).finally(()=>{
+    client.close();
+  })
+};
 module.exports={
   AddUser ,
   AddTasks ,
   GetUserName ,
   GetTasks ,
   GetUserByAge,
-  GetUserByEmail
+  GetUserByEmail,
+  UpdateTasks,
+  DeleteUsersByAge
 };
